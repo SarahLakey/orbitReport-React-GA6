663c2f1da4389c2d91639fd3c2e5cadc448956d0
SarahLakey <136642286+SarahLakey@users.noreply.github.com>
2023-07-19 11:00:11 -0500

    Working on Section 4 but getting errors still

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,30 @@
-const Buttons = () => {
-  return (
+import satData from "./satData";
+
+
+    //!  Part 4.2: Pass the props created from the App() function: filterByType, setSat, and displaySats.
+
+
+const Buttons = (props) => {
+  const filterByType = props.filterByType;
+  const setSat = props.setSat;
+  const displaySats = props.displaySats;
+
+    return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+        {displaySats.map((sat, id) => {
+          return (
+            <button onClick={() => filterByType(sat)} key={id}>
+              {sat} Orbit
+            </button>
+          );
+        })}
+
+      <button  onClick={() => setSat(setSat)}>
+        All Orbits</button>
       </div>
   );
 };
 
 export default Buttons;
+
+       //! Part 4.4 Inside the <button> tag, create an onClick function that points to setSat. Pass satData to setSat.
